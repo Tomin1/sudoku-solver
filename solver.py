128328c9611a72b162ac1c38b731bba1f0259494
Tomi Leppänen <tomppeli12@gmail.com>
2012-11-07 16:23:44 +0200

    The second release
    The same things (as said in commit before this) append to this version
    This version has a bug that may cause a deadlock.

--- solver.py
+++ solver.py
@@ -6,11 +6,12 @@
 # YOU ARE FORBIDDEN TO USE OR COPY THIS CODE (for now)!
 # © Tomi Leppänen (aka Tomin), 2011, ALL RIGHTS RESERVED!
 import sys
+from copy import deepcopy
 
-def get_grid(row,cell):
+def get_grid(row,cell): # checks which grid is being procecced
     return [(row+3)/3,(cell+3)/3]
 
-def isgood_final(sudoku): # checks if sudoku is correct
+def isgood_final(sudoku): # checks if sudoku is correct, only for completed
     for a in range(0,9):
         suma = 0
         sumb = 0
@@ -29,7 +30,7 @@ def isgood_final(sudoku): # checks if sudoku is correct
                 return False
     return True
 
-def isgood(sudoku): # checks if sudoku is correct
+def isgood(sudoku): # checks if sudoku is correct, slower
     for a in range(0,9):
         numbersa = []
         numbersb = []
@@ -59,7 +60,7 @@ def isgood(sudoku): # checks if sudoku is correct
                             numbersc.append(sudoku[r_n][c_n])
     return True
 
-def isready(sudoku): # checks if sudoku has all fields filled
+def isready(sudoku): # checks if all fields are filled
     for row in sudoku:
         try:
             row.index("")
@@ -68,7 +69,7 @@ def isready(sudoku): # checks if sudoku has all fields filled
             pass
     return True
 
-def parse_sudoku(sudoku): # parses array to sudoku
+def parse_sudoku(sudoku): # parses sudoku from array
     a = sudoku.split(",")
     sudoku = []
     for i in range(1,10):
@@ -86,7 +87,7 @@ def parse_sudoku(sudoku): # parses array to sudoku
         sudoku.append(b)
     return sudoku
 
-def parse_string(sudoku): # parses sudoku to array
+def parse_string(sudoku): # parses array from sudoku
     s = ""
     for row in sudoku:
         for cell in row:
@@ -96,44 +97,45 @@ def parse_string(sudoku): # parses sudoku to array
                 s = str(cell)
     return s
 
-def print_msg(message): # normal message
+def print_msg(message): # prints normal message
     sys.stdout.write(message)
     sys.stdout.write("\n")
 
-def print_err(message): # error message
+def print_err(message): # prints error message
     sys.stderr.write(message)
     sys.stderr.write("\n")
 
-def solve(sudoku): # solver function
-    for row_n in range(0,9):
-        for cell_n in range(0,9):
-            if sudoku[row_n][cell_n] == "":
-                numbers = range(1,10)
-                for n in range(0,9):
-                    if sudoku[row_n][n] != "":
+class Solver: # solver object
+    def __init__(self, sudoku, row, cell):
+        self.sudoku = sudoku
+        self.row = row
+        self.cell = cell
+    def run(self):
+        numbers = []
+        numbers.append(self.sudoku[self.row][self.cell])
+        if numbers[0] == "":
+            numbers = range(1,10)
+            for i in range(0,9):
+                try:
+                    numbers.remove(self.sudoku[self.row][i])
+                except ValueError:
+                    pass
+                try:
+                    numbers.remove(self.sudoku[i][self.cell])
+                except ValueError:
+                    pass
+            x,y = get_grid(self.row,self.cell)
+            for r in range(x*3-3,x*3):
+                for c in range(y*3-3,y*3):
+                    if self.sudoku[r][c] != "":
                         try:
-                            numbers.remove(sudoku[row_n][n])
+                            numbers.remove(self.sudoku[r][c])
                         except ValueError:
                             pass
-                for n in range(0,9):
-                    if sudoku[n][cell_n] != "":
-                        try:
-                            numbers.remove(sudoku[n][cell_n])
-                        except ValueError:
-                            pass
-                r,c = get_grid(row_n,cell_n)
-                for r_n in range(r*3-3,r*3):
-                    for c_n in range(c*3-3,c*3):
-                        if sudoku[r_n][c_n] != "":
-                            try:
-                                numbers.remove(sudoku[r_n][c_n])
-                            except ValueError:
-                                pass
-                if len(numbers) == 1:
-                    sudoku[row_n][cell_n] = numbers[0]
-    return sudoku
+        return numbers
 
 def main(argv):
+    # begin checks
     if len(argv) != 2:
         print_err("Usage: "+argv[0]+" sudoku as a string")
         return 2
@@ -141,34 +143,66 @@ def main(argv):
     if sudoku == -1:
         print_err("Invalid sudoku string!")
         return 2
-    old_sudoku = []
+    # begin solving
+    wsudoku = [] # array for wip sudokus
+    wsudoku.append(sudoku)
+    dsudoku = [] # array for sudokus that are done
     retries = RETRIES_DEFAULT = 3
-    while(not isready(sudoku)):
-        sudoku = solve(sudoku)
-        if old_sudoku == sudoku:
-            if not isgood(sudoku):
+    split_mode = False # if split mode is on or not :)
+    while(len(wsudoku) > 0):
+        changed = False
+        breaking = False
+        for cur in range(0,len(wsudoku)):
+            if isready(wsudoku[cur]): # if sudoku is solved it will be pushed
+                if isgood_final(wsudoku[cur]): # to dsudoku array and
+                    dsudoku.append(wsudoku[cur]) # removed from wsudoku array
+                    wsudoku.remove(wsudoku[cur])
+                    break
+                else:
+                    wsudoku.remove(wsudoku[cur])
+                    break
+            for row in range(0,9):
+                for col in range(0,9):
+                    if wsudoku[cur][row][col] == "":
+                        numbers = Solver(wsudoku[cur],row,col).run()
+                        if len(numbers) == 1:
+                            changed = True
+                            wsudoku[cur][row][col] = numbers[0]
+                        elif len(numbers) >= 2:
+                            if split_mode == True:
+                                split_mode = False
+                                for number in numbers:
+                                    wsudoku.append(deepcopy(wsudoku[cur]))
+                                    wsudoku[len(wsudoku)-1][row][col] = number
+                                wsudoku.remove(wsudoku[cur])
+                                breaking = True
+                                break
+                if breaking:
+                    break
+            if breaking:
                 break
-            retries -= 1
-        else:
-            retries = RETRIES_DEFAULT
-        if retries <= 0:
-            break
-        old_sudoku = sudoku
-    if not isready(sudoku):
-        print_msg("Sudoku was not solved!")
-        if not isgood(sudoku):
-            print_msg("Sudoku is faulty!")
-        retval = 1
-    elif not isgood_final(sudoku):
-        print_msg("Sudoku is not correct!")
-        retval = 1
+            if changed == False: # counts how many loops has gone without change
+                retries =- 1
+            else:
+                retries = RETRIES_DEFAULT
+            if retries <= 0: # checks for retries
+                if isgood(wsudoku[cur]):
+                    split_mode = True # turns split mode on
+                else:
+                    wsudoku.remove(wsudoku[cur])
+                    break
+    # printing results
+    if len(dsudoku) == 0:
+        print("Sudoku was not solved!")
+        return 1
     else:
-        print_msg("Sudoku is fine!")
-        retval = 0
-    for row in sudoku:
-        print(row)
-    print(parse_string(sudoku))
-    return retval
+        print("Answers: "+str(len(dsudoku)))
+        for wsudoku in dsudoku:
+            print_msg("One answer is:")
+            for row in wsudoku:
+                print_msg(str(row))
+            print_msg(parse_string(wsudoku))
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
